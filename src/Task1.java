c666de4906990cca5cacc765ca03bec70d0755e7
AKMERCAN <z.ucar@windowslive.com>
2019-11-17 15:22:45 -0500

    task 1 was uploaded

---
+++ src/Task1.java
@@ -0,0 +1,11 @@
+public class Task1 {
+    public int sum(int num1, int num2) {
+        int total = 0;
+        // write your code here//
+
+        total = num1 + num2;
+
+        // end of code////
+        return total;
+    }
+}
